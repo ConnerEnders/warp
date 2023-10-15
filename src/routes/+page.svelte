3f35720c61bf63670dec97eb85b586e4d853d398
Conner Enders <conner.enders@icloud.com>
2023-10-15 14:04:19 -0400

    Added scene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
+	import '../app.css';
+</script>
+
+<main>
+	<Canvas>
+		<Scene />
+	</Canvas>
+</main>
---
+++ src/routes/Scene.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+</script>
+
+<T.PerspectiveCamera makeDefault position={[2, 2, 2]}>
+	<OrbitControls />
+</T.PerspectiveCamera>
+<T.DirectionalLight />
+<T.Mesh>
+	<T.BoxGeometry />
+	<T.MeshNormalMaterial />
+</T.Mesh>
